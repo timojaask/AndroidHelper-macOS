7d8671b6973f94a99bf190ea18f5d0aa784006b7
Timo Jaaskelainen <timojaask@gmail.com>
2020-02-10 18:06:17 -0500

    Adjust window UI and terminate app on close

--- AndroidHelper-macOS/AppDelegate.swift
+++ AndroidHelper-macOS/AppDelegate.swift
@@ -1,26 +1,10 @@
-//
-//  AppDelegate.swift
-//  AndroidHelper-macOS
-//
-//  Created by Timo Jääskeläinen on 2/2/20.
-//  Copyright © 2020 Timo Jääskeläinen. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
-
